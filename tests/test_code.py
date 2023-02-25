5f2ae60164bc75d5d40402bcebae789cdb89e5f6
nandaUnical <rtgmfr98t59z504s@studenti.unical.it>
2023-02-25 12:06:05 +0100

    Primer unit test

--- tests/test_code.py
+++ tests/test_code.py
@@ -5,9 +5,9 @@ sys.path.insert(1,'C:/Users/hp/Documents/!Master/Agile/!!Second Apello/MARIA_FER
 
 
 def test_read_text_path_folder () :
-    a = code.read_text (r"C:\Users\hp\Documents\!Master\Agile\!!Second Apello\ejemplos")
+    a = read_text (r"C:\Users\hp\Documents\!Master\Agile\!!Second Apello\ejemplos")
     assert a == {'hola': 4, 'mundo': 3, 'gente': 2, 'la': 1}
 
 def test_read_text_list_files () :
-    a = code.read_text ([r"C:\Users\hp\Documents\!Master\Agile\!!Second Apello\ejemplos\texto_1.txt", r"C:\Users\hp\Documents\!Master\Agile\!!Second Apello\ejemplos\texto_2.txt", r"C:\Users\hp\Documents\!Master\Agile\!!Second Apello\ejemplos\texto_3.txt"])
+    a = read_text ([r"C:\Users\hp\Documents\!Master\Agile\!!Second Apello\ejemplos\texto_1.txt", r"C:\Users\hp\Documents\!Master\Agile\!!Second Apello\ejemplos\texto_2.txt", r"C:\Users\hp\Documents\!Master\Agile\!!Second Apello\ejemplos\texto_3.txt"])
     assert a == {'hola': 4, 'mundo': 3, 'gente': 2, 'la': 1}
