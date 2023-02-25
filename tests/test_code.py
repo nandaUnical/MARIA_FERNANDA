184edda3d8a205d195eda1c712aa87046b71647e
nandaUnical <rtgmfr98t59z504s@studenti.unical.it>
2023-02-25 11:19:24 +0100

    Primer unit test

--- tests/test_code.py
+++ tests/test_code.py
@@ -5,6 +5,6 @@ def test_read_text_path_folder () :
     a = code.read_text (r"C:\Users\hp\Documents\!Master\Agile\!!Second Apello\ejemplos")
     assert a == {}
 
-def test_read_text_list_files ()
+def test_read_text_list_files () :
     a = code.read_text ([r"C:\Users\hp\Documents\!Master\Agile\!!Second Apello\ejemplos\texto_1.txt", r"C:\Users\hp\Documents\!Master\Agile\!!Second Apello\ejemplos\texto_2.txt", r"C:\Users\hp\Documents\!Master\Agile\!!Second Apello\ejemplos\texto_3.txt"])
     assert a == {}
