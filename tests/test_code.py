a138af556e83e4c307c3dc985aa77c66031ad487
nandaUnical <rtgmfr98t59z504s@studenti.unical.it>
2023-02-25 11:08:36 +0100

    Primer unit test

--- tests/test_code.py
+++ tests/test_code.py
@@ -3,4 +3,4 @@ import code
 
 def test_read_text_path_folder () :
     a = read_text (r"C:\Users\hp\Documents\!Master\Agile\!!Second Apello\ejemplos")
-    assert if a , 'Diccionary is empty'
+    assert if a 
